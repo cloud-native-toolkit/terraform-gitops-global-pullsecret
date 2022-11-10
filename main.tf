3836c998d4531edff54d7526fdf037d619808e89
Andrew Trice <amtrice@us.ibm.com>
2022-11-10 09:54:34 -0500

    revert seal_secrets resource
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ locals {
   name          = local.secret_name
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/global-pull-secret"
   service_url   = "http://${local.name}.${var.namespace}"
-  source_dir      = "${path.cwd}/.tmp/source"
+  #source_dir      = "${path.cwd}/.tmp/source"
   tmp_dir      = "${path.cwd}/.tmp/tmp"
 
   values_content = {
@@ -31,7 +31,7 @@ resource null_resource create_yaml {
 
 resource null_resource create_secrets {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/create-secrets.sh '${local.secret_name}' '${local.source_dir}'"
+    command = "${path.module}/scripts/create-secrets.sh '${local.secret_name}' '${local.tmp_dir}'"
 
     environment = {
       DOCKER_PASSWORD = var.docker_password
@@ -40,15 +40,24 @@ resource null_resource create_secrets {
   }
 }
 
-resource gitops_seal_secrets secrets {
+# resource gitops_seal_secrets secrets {
+#   depends_on = [null_resource.create_secrets,null_resource.create_yaml]
+
+#   source_dir    = local.source_dir
+#   dest_dir      = "${local.yaml_dir}/templates"
+#   kubeseal_cert = var.kubeseal_cert
+#   tmp_dir = local.tmp_dir
+# }
+module seal_secrets {
   depends_on = [null_resource.create_secrets,null_resource.create_yaml]
 
-  source_dir    = local.source_dir
+  source = "github.com/cloud-native-toolkit/terraform-util-seal-secrets.git"
+
+  source_dir    = local.tmp_dir
   dest_dir      = "${local.yaml_dir}/templates"
   kubeseal_cert = var.kubeseal_cert
-  tmp_dir = local.tmp_dir
+  label         = local.secret_name
 }
-
 
 resource gitops_module setup_gitops {
   depends_on = [null_resource.create_yaml, gitops_seal_secrets.secrets]
