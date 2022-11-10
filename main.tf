557e4e9189fbe6c57c470ff025a0c6abe9828f71
Andrew Trice <amtrice@us.ibm.com>
2022-11-10 10:14:25 -0500

    put seal_secrets resource back
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ locals {
   default_secret_name = replace(replace("${var.docker_username}-${lower(var.docker_server)}", "/[^a-z0-9-.]/", "-"), "/-+/", "-")
   secret_name   = var.secret_name != "" ? var.secret_name : local.default_secret_name
   name          = local.secret_name
-  bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/global-pull-secret"
   service_url   = "http://${local.name}.${var.namespace}"
+  source_dir      = "${path.cwd}/.tmp/tmp"
   tmp_dir      = "${path.cwd}/.tmp/tmp"
 
   values_content = {
@@ -19,10 +19,6 @@ locals {
   layer_config = var.gitops_config[local.layer]
 }
 
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
-}
-
 resource null_resource create_yaml {
   provisioner "local-exec" {
     command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.yaml_dir}'"
@@ -35,7 +31,7 @@ resource null_resource create_yaml {
 
 resource null_resource create_secrets {
   provisioner "local-exec" {
-    command = "${path.module}/scripts/create-secrets.sh '${local.secret_name}' '${local.tmp_dir}'"
+    command = "${path.module}/scripts/create-secrets.sh '${local.secret_name}' '${local.source_dir}'"
 
     environment = {
       DOCKER_PASSWORD = var.docker_password
@@ -44,50 +40,25 @@ resource null_resource create_secrets {
   }
 }
 
-
-module seal_secrets {
+resource gitops_seal_secrets secrets {
   depends_on = [null_resource.create_secrets,null_resource.create_yaml]
 
-  source = "github.com/cloud-native-toolkit/terraform-util-seal-secrets.git"
-
-  source_dir    = local.tmp_dir
+  source_dir    = local.source_dir
   dest_dir      = "${local.yaml_dir}/templates"
   kubeseal_cert = var.kubeseal_cert
-  label         = local.secret_name
+  tmp_dir = local.tmp_dir
 }
 
 
-resource null_resource setup_gitops {
-  depends_on = [module.seal_secrets]
-
-  triggers = {
-    name = local.name
-    namespace = var.namespace
-    yaml_dir = local.yaml_dir
-    server_name = var.server_name
-    layer = local.layer
-    type = local.type
-    git_credentials = yamlencode(var.git_credentials)
-    gitops_config   = yamlencode(var.gitops_config)
-    bin_dir = local.bin_dir
-  }
-
-  provisioner "local-exec" {
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
-
-    environment = {
-      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
-      GITOPS_CONFIG   = self.triggers.gitops_config
-    }
-  }
+resource gitops_module setup_gitops {
+  depends_on = [null_resource.create_yaml, gitops_seal_secrets.secrets]
+  name = local.name
+  namespace = local.namespace
+  content_dir = local.yaml_dir
+  server_name = var.server_name
+  layer = local.layer
+  type = local.type
+  branch = local.application_branch
+  config = yamlencode(var.gitops_config)
+  credentials = yamlencode(var.git_credentials)
 }
