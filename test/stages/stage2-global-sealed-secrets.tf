65654e2a540162e281a3dedd42dba33864e235f4
Andrew Trice <amtrice@us.ibm.com>
2022-11-11 15:03:34 -0500

    updated namespace dependencies
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-global-sealed-secrets.tf
+++ test/stages/stage2-global-sealed-secrets.tf
@@ -4,7 +4,7 @@ module "global_sealed_secrets" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  namespace = module.gitops.namespace
+  namespace = var.namespace
   kubeseal_cert = module.gitops.sealed_secrets_cert
 
   docker_server = "test-server"
